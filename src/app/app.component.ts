34afa09337de43fea398c5b382e7a84e08f805ce
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-30 12:30:29 +0200

    'u'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bienvenue visiteur';
   nbClassicRooms = 4;
   nbVirtualRooms = 5;
   ageMin = 12;
   ageMax = 120;
+
+  ngOnInit() {
+    console.log('oninit');
+  }
 
   lastLi() {
     console.log('lastLi');
