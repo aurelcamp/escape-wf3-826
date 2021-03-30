633ce5a668c9b51e8fa1ac6bdcdfe1079b2d1554
aurelcamp <aureliencampistron@yahoo.fr>
2021-03-30 14:19:39 +0200

    'u'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidesComponent } from './components/slides/slides.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidesComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
