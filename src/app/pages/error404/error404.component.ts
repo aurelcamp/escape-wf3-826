4028cab02e6272c26db5674b846553f94b81699a
aurelcamp <aureliencampistron@yahoo.fr>
2021-04-02 13:42:36 +0200

    'u'

--- src/app/pages/error404/error404.component.ts
+++ src/app/pages/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error404',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
+    setTimeout(
+      () => {
+        this.router.navigate(['/rooms']);
+      },
+      5000
+    )
   }
 
 }
